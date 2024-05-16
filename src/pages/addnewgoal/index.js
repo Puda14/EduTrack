f607c1e8e6a6ef06ab87b5c64f66bb339b28c539
tduckie_ <golden4gnvx@gmail.com>
2024-05-16 22:30:13 +0700

    basic layout of add new goal page

--- src/pages/addnewgoal/index.js
+++ src/pages/addnewgoal/index.js
@@ -1,29 +1,44 @@
+import KPITabs from "@/app/components/kpi/KPITabs";
 import { Input } from "@nextui-org/input";
 import { DateInput } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
+import { useState } from "react";
 
 const AddNewGoalPage = () => {
+  const [list, setList] = useState([]);
+
+  const handleAddToList = (item) => {
+    setList;
+  };
+  // list contains goals
+  // list = [ {task = [{name, type, fromDate, toDate, description} ], title, fromDate, toDate, role, hashtag, description} ]
+  //
   return (
     <div className="flex-container flex flex-col justify-between">
-      <div>
-        <h2>New Goal</h2>
-        <div>Title</div>
-        <Input />
-        <div>From</div>
-        <DateInput />
-        <div>To</div>
-        <DateInput />
-        <div>Role</div>
-        <Input />
-        <div>Hashtag</div>
-        <Input />
-        <div>Description</div>
-        <Input />
-        <Button />
+      <div classname="goal-menu flex flex-row item-end">
+        <form
+          onSubmit={handleAddToList}
+          classname="goal-menu flex flex-row w-full"
+        >
+          <h2>New Goal</h2>
+          <div>Title</div>
+          <Input />
+          <div>From</div>
+          <DateInput />
+          <div>To</div>
+          <DateInput />
+          <div>Role</div>
+          <Input />
+          <div>Description</div>
+          <Input />
+        </form>{" "}
+        <Button>Add Goal</Button>
       </div>
-      <div>
+      <div className="kpi-menu flex flex-row item-center">
         <h2>KPI</h2>
-        <Button />
+        <KPITabs />
+        <KPITabs />
+        <Button>ADD NEW KPI</Button>
       </div>
     </div>
   );

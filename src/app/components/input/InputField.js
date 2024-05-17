6111462d0df8caac7a724b25d6acc531eaf10e9a
tduckie_ <golden4gnvx@gmail.com>
2024-05-17 14:43:33 +0700

    temporary inline styling

--- src/app/components/input/InputField.js
+++ src/app/components/input/InputField.js
@@ -2,14 +2,19 @@ import { Input } from "@nextui-org/input";
 import React, { useState } from "react";
 export const InputField = (props) => {
   const [value, setValue] = useState("");
-  const { name, style } = props;
+  const { name, style, placeholder } = props;
   const handleChange = (e) => {
     console.log(`${name} field value: ${value}`);
     setValue(e.target.value);
   };
   return (
-    <>
-      <Input onChange={handleChange} value={value} style={style} />
-    </>
+    <div style={{ margin: "8px 0" }}>
+      <Input
+        onChange={handleChange}
+        value={value}
+        style={style}
+        placeholder={placeholder}
+      />
+    </div>
   );
 };
--- src/pages/addnewgoal/index.js
+++ src/pages/addnewgoal/index.js
@@ -1,5 +1,7 @@
 import { ClipboardIcon } from "@/app/components/icons/ClipboardIcon";
 import { InputField } from "@/app/components/input/InputField";
+import { DateInputField } from "@/app/components/input/DateInputField";
+import { SelectInputField } from "@/app/components/input/SelectInputField";
 import KPITabs from "@/app/components/kpi/KPITabs";
 import { Input } from "@nextui-org/input";
 import {
@@ -38,7 +40,7 @@ const AddNewGoalPage = () => {
           onSubmit={handleAddToList}
           classname="goal-menu flex flex-row w-full"
         >
-          <h2>New Goal</h2>
+          <h2 style={{ fontSize: 34 }}>New Goal</h2>
           <div>Title</div>
           {/* <Input
             style={{ border: "none" }}
@@ -46,11 +48,14 @@ const AddNewGoalPage = () => {
             onChange={handleInputChange}
             value={input}
           /> */}
-          <InputField name="Title" style={{ border: "none" }} />
+          <InputField
+            name="Title"
+            style={{ border: "none", margin: "8px 0" }}
+          />
           <div>From</div>
-          <DateInput />
+          <DateInputField />
           <div>To</div>
-          <DateInput />
+          <DateInputField />
           <div>Role</div>
           {/*<Inpu t
             style={{ border: "none" }}
@@ -59,21 +64,42 @@ const AddNewGoalPage = () => {
           /> */}
           <InputField name="Role" style={{ border: "none" }} />
           <div>Description</div>
-          <Input
+          <InputField
             style={{ border: "none" }}
             placeholder="Please enter description (/250)"
           />
         </form>{" "}
-        <Button>Add Goal</Button>
+        <Button style={{ margin: "32px 0" }} color="primary">
+          Add Goal
+        </Button>
       </div>
       <div
         className="kpi-menu flex flex-col item-center"
         style={{ width: 320 }}
       >
-        <h2>KPI</h2>
-        <Card>
-          <CardBody>
-            <p>Goal 1</p>
+        <h2 style={{ fontSize: 34 }}>KPI</h2>
+        <Card
+          style={{ backgroundColor: "#dad0ff", margin: "16px 0" }}
+          className="flex-row"
+        >
+          <div className="flex items-center justify-center h-full">
+            <ClipboardIcon />
+          </div>
+          <CardBody className="bg-white">
+            <div>Task 1</div>
+            <div>Example task</div>
+          </CardBody>
+        </Card>
+        <Card
+          style={{ backgroundColor: "#dad0ff", margin: "16px 0" }}
+          className="flex-row"
+        >
+          <div className="flex items-center justify-center h-full">
+            <ClipboardIcon />
+          </div>
+          <CardBody className="bg-white">
+            <div>Task 2</div>
+            <div>Example task</div>
           </CardBody>
         </Card>
         <Button
@@ -88,22 +114,22 @@ const AddNewGoalPage = () => {
             {() => (
               <>
                 <ModalBody>
-                  <h1>New KPI</h1>
+                  <h1 style={{ fontSize: 34 }}>New KPI</h1>
                   <div
                     className="flex w-full justify-between px-8"
                     style={{ gap: 120 }}
                   >
                     <div className="w-1/2">
                       <div>KPI Title</div>
-                      <Input style={{ border: "none" }} />
+                      <InputField style={{ border: "none" }} />
                     </div>
                     <div className="w-1/2">
                       <div>KPI Description</div>
-                      <Input style={{ border: "none" }} />
+                      <InputField style={{ border: "none" }} />
                     </div>
                   </div>
                   <hr />
-                  <h1>Tasks</h1>
+                  <h1 style={{ fontSize: 34 }}>Tasks</h1>
                   <div
                     className="flex w-full justify-between px-8"
                     style={{ gap: 120 }}
@@ -117,22 +143,21 @@ const AddNewGoalPage = () => {
                         <Button color="primary">Export</Button>
                       </div>
                       <div>Task name</div>
-                      <Input style={{ border: "none" }} />
+                      <InputField style={{ border: "none" }} />
                       <div>Type</div>
-                      <Select>
-                        <SelectItem>Required</SelectItem>
-                        <SelectItem>Optional</SelectItem>
-                      </Select>
+                      <SelectInputField />
                       <div>From</div>
-                      <DateInput />
+                      <DateInputField />
                       <div>To</div>
-                      <DateInput />
+                      <DateInputField />
                       <div>Description</div>
-                      <Input style={{ border: "none" }} />
-                      <Button color="primary">ADD NEW TASK</Button>
+                      <InputField style={{ border: "none" }} />
+                      <Button style={{ marginTop: "12px" }} color="primary">
+                        ADD NEW TASK
+                      </Button>
                     </div>
                     <div className="w-1/2">
-                      <h2>Required Tasks</h2>
+                      <h2 style={{ fontSize: 34 }}>Required Tasks</h2>
                       <Card
                         style={{ backgroundColor: "#dad0ff", margin: "16px 0" }}
                         className="flex-row"
@@ -158,7 +183,7 @@ const AddNewGoalPage = () => {
                         </CardBody>
                       </Card>
                       <hr />
-                      <h2>Optional Tasks</h2>
+                      <h2 style={{ fontSize: 34 }}>Optional Tasks</h2>
                       <Card
                         style={{ backgroundColor: "#dad0ff", margin: "16px 0" }}
                         className="flex-row"
